481f5f163036ff5488bb0838d3ad10d05bc49c2f
Ihor <igxr.lxgvenyuk@gmail.com>
2024-08-08 16:42:57 +0300

    second exercise

--- script.js
+++ script.js
@@ -8,9 +8,52 @@ const personalMovieDB = {
     privat: false
 }
 
-let lastFilm = prompt('Last film u watched?'),
+let lastFilm, filmRating, i = 1;
+
+// while(i <= 3) {
+//     lastFilm = prompt('Last film u watched?');
+//     filmRating = prompt('this film rating');
+
+//     if (lastFilm == '' || lastFilm == null || lastFilm.length > 50 || filmRating == '' || filmRating == null || filmRating.length > 50) {
+//         alert('try again');
+//     } else {
+//         personalMovieDB.movies[lastFilm] = filmRating;
+//         i++
+//     }
+// }
+
+// do {
+//     lastFilm = prompt('Last film u watched?');
+//     filmRating = prompt('this film rating');
+
+//     if (lastFilm == '' || lastFilm == null || lastFilm.length > 50 || filmRating == '' || filmRating == null || filmRating.length > 50) {
+//         alert('try again');
+//     } else {
+//         personalMovieDB.movies[lastFilm] = filmRating;
+//         i++
+//     }
+// } while(i <= 3)
+
+for (i; i <= 3; i++) {
+    lastFilm = prompt('Last film u watched?');
     filmRating = prompt('this film rating');
 
-personalMovieDB.movies[lastFilm] = filmRating;
+    if (lastFilm == '' || lastFilm == null || lastFilm.length > 50 || filmRating == '' || filmRating == null || filmRating.length > 50) {
+        alert('try again');
+        i--;
+    } else {
+        personalMovieDB.movies[lastFilm] = filmRating;
+    }
+}
+
+if (personalMovieDB.count <= 10) {
+    alert("small amount of films");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    alert("good amount of films");
+} else if (personalMovieDB.count > 30) {
+    alert("big amount of films");
+} else {
+    alert("error");
+}
 
 console.log(personalMovieDB);
